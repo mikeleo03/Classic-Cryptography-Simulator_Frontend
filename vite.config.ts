e3fa7f8f38ab340de2e837c37ade018b770c376b
mikeleo03 <leonmichael463@gmail.com>
2024-02-15 21:35:23 +0700

    feat : restructure the program, relocate main port, and ready for dev!

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,15 @@ import { defineConfig } from "vite"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    strictPort: true,
+    port: 3000,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+    extensions: ['.tsx', '.ts', '.jsx', '.js'],
   },
 })
